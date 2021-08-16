061c4eb51033942a51824af4597a201c9729c081
dymafr <erwan1309@hotmail.com>
2021-08-16 16:46:44 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,39 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import {
   FormArray,
   FormBuilder,
   FormControl,
   FormGroup,
   Validators
-} from "@angular/forms";
-import { Subscription } from "rxjs";
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, OnDestroy {
-  public form: FormGroup;
+  public form: FormGroup = this.fb.group({
+    nom: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    hobbies: this.fb.array([]),
+    password: ['']
+  });
   public subscriptions = new Subscription();
 
   constructor(private fb: FormBuilder) {}
 
   get hobbies() {
-    return this.form.get("hobbies") as FormArray;
+    return this.form.get('hobbies') as FormArray;
   }
 
   ngOnInit() {
-    this.createForm();
-
     this.subscriptions.add(
       this.form.statusChanges.subscribe(status => {
         console.log(status);
       })
     );
-
     this.subscriptions.add(
       this.form.valueChanges.subscribe(value => {
         console.log(value);
@@ -39,17 +41,8 @@ export class AppComponent implements OnInit, OnDestroy {
     );
   }
 
-  createForm() {
-    this.form = this.fb.group({
-      nom: ["", Validators.required],
-      email: ["", [Validators.required, Validators.email]],
-      hobbies: this.fb.array([]),
-      password: [""]
-    });
-  }
-
   addHobby() {
-    this.hobbies.push(this.fb.control(""));
+    this.hobbies.push(this.fb.control(''));
   }
 
   deleteHobby(index: number) {
